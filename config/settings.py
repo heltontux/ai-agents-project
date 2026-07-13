82a0cdfe861d97c6a7dae58bdac22a14ba0076f3
Helto Augusto <heltontux@gmail.com>
2026-07-13 15:53:18 -0300

    refactor: adding the OpenAI model as a variable

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-5")
 
 if not OPENAI_API_KEY:
     raise ValueError("A variável OPENAI_API_KEY não foi encontrada.")
--- services/openai_service.py
+++ services/openai_service.py
@@ -1,13 +1,16 @@
 from openai import OpenAI
 
-from config.settings import OPENAI_API_KEY
+from config.settings import (
+        OPENAI_API_KEY,
+        OPENAI_MODEL,
+    )
 
 class OpenAIService:
     def __init__(self):
         self.client = OpenAI(api_key=OPENAI_API_KEY)
     def ask(self, prompt: str):
         response = self.client.responses.create(
-            model="gpt-5",
+            model=OPENAI_MODEL,
             input=prompt,
         )
 
